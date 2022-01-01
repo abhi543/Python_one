8cdaeb07e68c7a7c1132495a4408128975832417
abhi543 <ak0787777@gmail.com>
2022-01-01 16:17:35 +0530

    day 1

---
+++ calli.py
@@ -0,0 +1,5 @@
+# calendar of the month 
+import calendar 
+cal=calendar.month(2022,1)
+print("here is the calendar:  ")
+print(cal)
---
+++ combine_strings.py
@@ -0,0 +1,4 @@
+# combine strings
+x=input('enter your name: ')
+y=input('enter your password ')
+print(" ". join([x, y]))
